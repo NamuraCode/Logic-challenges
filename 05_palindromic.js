8c997e2758a3410fb72887c949c2622f5fdb0f76
David <dnaranjo0@misena.edu.co>
2022-10-26 23:09:20 -0500

    palindromic

--- 05_palindromic.js
+++ 05_palindromic.js
@@ -1,26 +1,18 @@
-let maximos = 99
-let validation = (number)=>{
-    let resultadoArray
-    let max = number.length
-    if (max > 3) {
-        resultadoArray = `${number[0]}` + `${number[1]}`
-    }
-    // console.log(resultadoArray);
-}
+let palindromicoTowNumbers = 99
+let palindromicoThreeNumbers = 999
 
-for (let i = 1; i <= 99; i++) {
-    for (let j = 1; j < 99; j++) {
-        // console.log(i * (j))
-        // console.log(`${i} this is the number ${j}`);
-        let result = `${i * (j)}`
-        validation(result)
-
+function findPalindromico(number){
+    if (number.toString().length <= 2 ) {
+        for (let i = 10; i <= 99; i++) {
+            for (let j = 10; j <= 99; j++) {
+                let polindromo = i*j
+                let polindromoString = polindromo.toString()
+                if (polindromoString.length % 2 == 0) {
+                    console.log(polindromoString);
+                }
+            }
+        }
     }
 }
 
-const i = "9009";
-const j = Array.from(i.split("").reverse());
-const array = i.split("");
-if (JSON.stringify(j) === JSON.stringify(array)) {
-  console.log("si");
-}
+findPalindromico(palindromicoTowNumbers)
